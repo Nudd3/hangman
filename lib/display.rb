50b9a3ac5c108803f88b493b60378ef04f559102
Noak Pettersson <noake12@gmail.com>
2022-07-23 16:14:39 +0200

    Added method to display guesses colored

--- lib/display.rb
+++ lib/display.rb
@@ -3,7 +3,6 @@
 # module containing methods for displaying info
 # for the player
 module Display
-
   def display_turn(word, guess)
     to_show = Array.new(word.length, '_')
     word.each_index do |i|
@@ -13,8 +12,30 @@ module Display
     puts to_show.join
   end
 
+  def display_guessed_letters(letters)
+    correct = letters['correct']
+    wrong = letters['wrong']
+    correct.each do |letter|
+      print "#{green(letter)} "
+    end
+    wrong.each do |letter|
+      print "#{red(letter)} "
+    end
+  end
+
+  def red(letter)
+    "\e[31m#{letter}\e[0m"
+  end
+
+  def green(letter)
+    "\e[32m#{letter}\e[0m"
+  end
 end
 
 include Display
+a = {
+  'correct' => %w[a b c e f],
+  'wrong' => %w[x c z b]
+}
 
-display_turn('call'.split(//), 'cole'.split(//))
+display_guessed_letters(a)
