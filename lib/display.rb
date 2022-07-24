5338dea898500e316ece18ee9b7dc6d61f058ded
Noak Pettersson <noake12@gmail.com>
2022-07-24 03:51:45 +0200

    Fixed formatting for display methods

--- lib/display.rb
+++ lib/display.rb
@@ -3,23 +3,22 @@
 # module containing methods for displaying info
 # for the player
 module Display
-  def display_turn(word, guess)
-    to_show = Array.new(word.length, '_')
-    word.each_index do |i|
-      to_show[i] = word[i] if word[i] == guess[i]
-    end
 
-    puts to_show.join
+  def display_info(letters, word_array)
+    display_guessed_letters(letters)
+    puts "\n\n"
+    puts "  #{word_array.join(' ')}"
+    
   end
 
   def display_guessed_letters(letters)
     correct = letters['correct']
     wrong = letters['wrong']
     correct.each do |letter|
-      print "#{green(letter)} "
+      print "  #{green(letter)}"
     end
     wrong.each do |letter|
-      print "#{red(letter)} "
+      print "  #{red(letter)}"
     end
   end
 
