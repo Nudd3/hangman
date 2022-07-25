5d6d801ade9d61c5ce749ce0e11475dd88d36767
Noak Pettersson <noake12@gmail.com>
2022-07-25 17:04:33 +0200

    Moved save methods into file_mananger.rb. load method not yet done

--- lib/file_manager.rb
+++ lib/file_manager.rb
@@ -4,4 +4,21 @@
 # a game of Hangman
 module FileManager
 
+  def save_game
+    filename = "#{@word_array.join}_game.json"
+    Dir.mkdir('saved_games') unless File.exist?('saved_games')
+    File.open("saved_games/#{filename}", 'w') { _1.write save_to_json}
+    puts goodbye_message(filename)
+  end
+
+  def save_to_json
+    JSON.pretty_generate(
+      word: @word,
+      word_array: @word_array,
+      guessed_letters: @guessed_letters,
+      guesses: @guesses
+    )
+  end
+
+  def load_game; end
 end
